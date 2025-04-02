1b321c26838f806d387d3d14ee7c32a4b6ac10c1
Tariq Ali <tarikali7444@gmail.com>
2025-04-02 11:22:36 +0530

    changes button bg color

--- src/component/ui/button.jsx
+++ src/component/ui/button.jsx
@@ -5,7 +5,7 @@ const ButtonCard = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-blue-600 cursor-pointer hover:bg-blue-700 text-white rounded-full flex items-center justify-center gap-1 px-4 py-2.5"
+      className="bg-gradient-to-r from-[#103ce7] to-[#64e9ff] text-white rounded-full flex items-center justify-center gap-1 px-4 py-2.5"
     >
       Contact Us <ArrowRight className="ml-2 h-4 w-4" />
     </button>
